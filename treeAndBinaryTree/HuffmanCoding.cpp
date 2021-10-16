46fa57ebfc5bf02e5cd3cf46561204d0aca09f3d
望舒 <62752455+Fentaniao@users.noreply.github.com>
2021-10-16 18:10:02 +0800

    Update

--- treeAndBinaryTree/HuffmanCoding.cpp
+++ treeAndBinaryTree/HuffmanCoding.cpp
@@ -1,230 +1,229 @@
-/*5.3 综合设计----哈夫曼编码    //也可作为最后一次的综合设计型实验
+/*
+5.3 综合设计----哈夫曼编码    //也可作为最后一次的综合设计型实验
 【实验目的】
 了解哈夫曼树的结构特点及有关概念，掌握哈夫曼树的建立和哈夫曼编码的设计
 【问题描述】
 使用二叉树进行哈夫曼编码的设计，要求对输入的一串电文字符实现哈夫曼编码，再对哈夫编码生成的代码串进行译码，输出电文字符串
 【实验功能】
-1、	哈夫曼树的建立   从终端读入字符集大小n，以及n个字符和n个权值，建立哈夫曼树
+1、哈夫曼树的建立   从终端读入字符集大小n，以及n个字符和n个权值，建立哈夫曼树
 2、	哈夫曼编码的生成  利用以建好的哈夫曼树，规定左分支为0，右分支为1，则从根结点到每个叶子结点所经过的分支对应的0和1组成的序列便为该结点对应字符的哈夫曼编码
 【设计思想】
-设计步骤
+设计步骤1)
 1、从没有父结点的结点集合中，找出2个权值最小的结点，求和后存放到新的链表单元中，并将找到的最小2个权值的结点的父结点地址指向新生成的结点。
 2、重复步骤（1），直至循环完成。
 3、从叶子结点遍历父结点，并记录遍历路径（当前结点为父结点的左儿子记为0，否则记为1），然后对遍历路径进行倒置处理，便得到该结点的哈夫曼编码。
 4、重复步骤（3），直至完成全部叶子结点的哈夫曼编码。
-【测试数据】
-用下表给出的字符集和频度的实际统计数据建立哈夫曼树，并实现以下报文的编码和译码："CAS；CAT；SAT；AT"
+ 【测试数据】
+用下表给出的字符集和频度的实际统计数据建立哈夫曼树，并实现以下报文的编码和译码：“CAS；CAT；SAT；AT”
 字符	C	A	S	T	;
 频度	2	4	2	3	3
-【具体参考】
-教材 P136--142 5.7节
 */
 
 #include <iostream>
-#include <cstring>
+#include <algorithm>
 
-typedef int status;
 using namespace std;
+
+//定义
 const int OK = 1;
 const int ERROR = 0;
-const int OVERFLOW = -2;
+const int OVERFOLOW = -2;
 const int MAXSIZE = 100;
 
-typedef struct BiTNode {
-    float data;
-    struct BiTNode *parent, *lchild, *rchild;
-} BiTNode, *BiTree;
-typedef struct {
-    char data;
-    float fre;
-    BiTNode *password;
-    string word;
-} charNode;
+typedef int status;
+typedef float elemType;
 
-int Getch(charNode B[], char ch, int &number, int &chnumber) {
-    int i = 0;
-    for (; i < number; i++)
-        if (B[i].data == ch) {
-            B[i].fre++;
-            chnumber = i;
-            return 0;
-        }
-    return 1;
+//二叉树类
+typedef struct biTNode {
+    elemType weight;
+    biTNode *parent, *lChild, *rChild;
+} biTNode, *biTree;
+
+typedef biTNode linkListElemType;
+
+//链表类
+typedef struct LNode {
+    linkListElemType *data;
+    struct LNode *next;
+} LNode, *LinkList;
+
+//初始化链表
+status initList(LinkList &L) {
+    L = new LNode;
+    L->next = nullptr;
+    return OK;
 }
 
-void OUT(charNode A[], int number) {
-    int i = 0;
-    for (; i < number; i++)
-        cout << A[i].data << A[i].fre << endl;
+//在链表的末尾添加元素
+status append(LinkList &L, linkListElemType *input) {
+    LNode *p = L;
+
+    while (p->next)
+        p = p->next;
+
+    LNode *q;
+    q = new LNode;
+    q->data = input;
+    q->next = nullptr;
+
+    p->next = q;
+    return OK;
 }
 
-void leave(BiTree &X, int i, charNode A) {
-    BiTNode *Y;
-    Y = new BiTNode;
-    X = new BiTNode;
-    Y->lchild = NULL;
-    Y->rchild = NULL;
-    Y->data = i;
-    Y->parent = X;
-    X->data = A.fre;
-    X->lchild = Y;
-    X->rchild = NULL;
-    X->parent = NULL;
+//获取链表的表长
+status length(LinkList &L) {
+    LNode *p = L;
+    int len = 0;
+
+    while (p->next) {
+        p = p->next;
+        len = len + 1;
+    }
+    return len;
 }
 
-void createtree(BiTree &L, BiTree &R) {
-    BiTNode *q;
-    q = new BiTNode;
-    q->lchild = L;
-    q->rchild = R;
-    q->data = L->data + R->data;
-    L->parent = q;
-    R->parent = q;
-    L = q;
+//链表的删除操作，删除表中第i个元素
+status deleteElem(LinkList &L, int i) {
+    LNode *p = L;
+    int j = 1;
+
+
+    if (i < 1 || length(L) < i)
+        return ERROR;
+    while (j < i) {
+        p = p->next;
+        j++;
+    }
+
+    p->next = p->next->next;
+
+    return OK;
 }
 
-//
-void password(BiTree &Q, string &str, float &depth) {
-    BiTNode *q;
-    q = Q;
-    str = "";
-    int i = 0;
-    while (q->parent != NULL) {
-        if (q->parent->lchild == q)
-            str = "0" + str;
-        if (q->parent->rchild == q)
-            str = "1" + str;
-        i++;
-        q = q->parent;
+//生成哈夫曼树
+void createBiTree(int &set_size, LinkList &L) {
+    int i;
+    LNode *p;
+    float min1, min2;
+    int index_min1, index_min2;
+    biTNode *t, *t1, *t2;
+
+    while (length(L) > 1) {
+//        查找链表中最小值的下标
+        p = L->next;
+        min1 = min2 = 999;
+        index_min1 = index_min2 = 0;
+        t1 = t2 = p->data;
+
+        for (i = 0; i < length(L); i++) {
+
+            if (p->data->weight < min1) {
+                min1 = p->data->weight;
+                index_min1 = i;
+                t1 = p->data;
+            }
+            p = p->next;
+        }
+//        查找链表中次小值的下标
+        p = L->next;
+        for (i = 0; i < length(L); i++) {
+
+            if ((p->data->weight < min2) && (i != index_min1)) {
+                min2 = p->data->weight;
+                index_min2 = i;
+                t2 = p->data;
+            }
+            p = p->next;
+        }
+
+//        组成新结点
+        t = new biTNode;
+        t->weight = min1 + min2;
+        t->parent = nullptr;
+        t->lChild = t2;
+        t->rChild = t1;
+        t2->parent = t;
+        t1->parent = t;
+
+//        从链表中移除两个用过的结点
+        deleteElem(L, index_min2 + 1);
+        deleteElem(L, index_min1 + 1);
+
+//        添加新结点到链表的末尾
+        append(L, t);
     }
-    depth = i;
 }
 
-//
-void printbtree(BiTree &T) {
-    if (T != NULL)
-        cout << T->data;
-    if (T->lchild != NULL || T->rchild != NULL) {
-        cout << "(";
-        if (T->lchild != NULL) printbtree(T->lchild);
-        if (T->rchild != NULL) {
-            cout << ",";
-            printbtree(T->rchild);
-        }
-        cout << ")";
+//求某个结点的哈夫曼编码
+string singleHuffmanCoding(biTNode *T) {
+    string code;
+    while (T->parent) {
+        if (T->parent->lChild == T)
+            code += "0";
+        else
+            code += "1";
+        T = T->parent;
     }
-}
-
-//
-int getstring(int getint) {
-    char ch[MAXSIZE];
-    int chnumber[MAXSIZE];
-    int length = 0;
-    int number = 1;
-    int i, j;
-    charNode A[MAXSIZE];
-    if (getint) {
-        cout << "输入字符串：" << endl;
-        cin >> ch[++length];
-        A[0].data = ch[length];
-        A[0].fre = 1;
-        for (;;) {
-            if ((ch[++length] = cin.get()) != '\n') {
-                if (Getch(A, ch[length], number, chnumber[length])) {
-                    A[number].data = ch[length];
-                    A[number++].fre = 1;
-                    chnumber[length] = number - 1;
-                }
-            } else break;
-        }
-    }
-    if (getint == 0) {
-        cout << "输入字符：" << endl;
-        number = 0;
-        char ch;
-        ch = '0';
-        while (ch != '\n') {
-            cin >> A[length++].data;
-            ch = cin.get();
-        }
-        cout << "输入概率：" << endl;
-        ch = '0';
-        while (ch != '\n') {
-            cin >> A[number++].fre;
-            ch = cin.get();
-        }
-        if (length != number) {
-            cout << "个数不匹配";
-            return 0;
-        }
-    }
-//建树
-    BiTree T[number];
-    for (i = 0; i < number; i++) {
-        leave(T[i], i, A[i]);
-        A[i].password = T[i];
-    }
-    int inumber = number;
-    do {
-        for (i = 0; i < inumber; i++)
-            for (j = i; j < inumber; j++)
-                if (T[i]->data > T[j]->data) {
-                    BiTNode *q;
-                    q = T[i];
-                    T[i] = T[j];
-                    T[j] = q;
-                }
-        createtree(T[0], T[1]);
-        T[1] = T[--inumber];
-    } while (inumber > 1);
-    for (i = 0; i < number; i++)
-        password(A[i].password, A[i].word, A[i].password->data);
-    if (getint) {
-        int sum = 0;
-        for (i = 0; i < number; i++)
-            sum += A[i].password->data * A[i].fre;
-        cout << "带权路径长度" << sum << endl;
-        for (i = 0; i < number; i++)
-            cout << A[i].data << A[i].word << endl;
-        cout << "密码:" << endl;
-        for (i = 1; i < length; i++)
-            cout << A[chnumber[i]].word;
-        cout << endl;
-    }
-    if (getint == 0) {
-        for (i = 0; i < number; i++)
-            cout << A[i].data << A[i].word << endl;
-        {
-            cout << "输入字符串:" << endl;
-            int k = 0;
-            char ch[MAXSIZE];
-            cin >> ch[k++];
-            while ((ch[k] = cin.get()) != '\n') k++;
-            cout << "密码:" << endl;
-            for (i = 0; i < k; i++)
-                for (j = 0; j < number; j++)
-                    if (ch[i] == A[j].data)
-                        cout << A[j].word;
-            cout << endl;
-        }
-    }
-    return 1;
+    reverse(code.begin(), code.end());
+    return code;
 }
 
 int main() {
-    while (true) {
-        string i;
-        cout << "0：已知概率求字符串\n"
-                "1:直接求字符串\n";
-        cin >> i;
-        if (i == "0" || i == "1") {
-            getstring(i[0] - 48);
-            break;
-        } else
-            cout << "输入错误" << endl;
+    int i;
+    int set_size;
+
+    cout << "读入字符集大小：";
+    cin >> set_size;
+
+    string arrayChar;
+    biTNode *arrayBiTNode[set_size];
+    string arrayCode[set_size];
+    LinkList L;
+    initList(L);
+
+    cout << "读入字符集：";
+    cin >> arrayChar;
+
+//    读入权值构建结点
+    cout << "读入权值：";
+    for (i = 0; i < set_size; i++) {
+        biTNode *t;
+        t = new biTNode;
+        cin >> t->weight;
+        t->parent = t->lChild = t->rChild = nullptr;
+        arrayBiTNode[i] = t;
+        append(L, t);
+    }
+
+    cout << "正在建立哈夫曼树" << endl;
+    createBiTree(set_size, L);
+    cout << "哈夫曼树创建成功！" << endl;
+
+    cout << "正在建立编码" << endl;
+    for (i = 0; i < set_size; i++) {
+        arrayCode[i] = singleHuffmanCoding(arrayBiTNode[i]);
+    }
+
+    for (i = 0; i < set_size; i++) {
+        cout << arrayChar[i] << " " << arrayCode[i] << endl;
+    }
+    cout << "编码创建成功！" << endl;
+
+    cout << "读入报文：";
+    string telegraph;
+    cin >> telegraph;
+
+    cout << "输出编码：";
+    for (i = 0; i < telegraph.length(); i++) {
+        auto index = arrayChar.find(telegraph[i]);
+        if (index != string::npos)
+            cout << arrayCode[index];
+        else
+            cout << telegraph[i];
     }
 }
-//0
-//C A S T ;
-//2 4 2 3 3
-//CAST;CA;SAT;AT
+// 5
+// CAST;
+// 2 4 2 3 3
+// CAST;CA;SAT;AT
+// 00101000111000101100000111100111
