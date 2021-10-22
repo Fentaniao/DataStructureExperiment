eb02c3762c53cd984511e1835eb322035686ab88
望舒 <62752455+Fentaniao@users.noreply.github.com>
2021-10-22 13:49:39 +0800

    Update PowerTrace.cpp

--- PowerTrace.cpp
+++ PowerTrace.cpp
@@ -20,17 +20,44 @@ int main() {
 
     cout << "INFO 2变量" << endl;
     for (i = 0; i < 3; ++i) {
-        trace(indexV1, i);
-        traceArr(weight, i);
+        trace(indexV1, list<int>{i,2});
+        traceArr(weight, list<int>{i});
     }
     cout<<endl;
 
     cout << "INFO 3变量" << endl;
     for (i = 0; i < 3; ++i) {
-        trace(indexV1, i, "这是一段关于数值变量的描述");
-        traceArr(weight, i, "这是一段关于数组变量的描述");
+        trace(indexV1, list<int>{i,i}, "这是一段关于数值变量的描述");
+        traceArr(weight, list<int>{i,3}, "这是一段关于数组变量的描述");
     }
     cout<<endl;
+
+
+    cout << "INFO trace 代码生成" << endl;
+    for (i = 1; i < 9; i++) {
+        cout << "#define TRACE_" << i << "(v";
+        for (j = 1; j < i; j++) {
+            cout << ", opt" << j;
+        }
+        cout << ") Trace(v, #v, __LINE__, __FUNCTION__";
+        for (j = 1; j < i; ++j) {
+            cout << ", " << "opt" << j;
+        }
+        cout << ")" << endl;
+    }
+
+    cout << "INFO traceArr 代码生成" << endl;;
+    for (i = 1; i < 9; i++) {
+        cout << "#define TRACEARR_" << i << "(v";
+        for (j = 1; j < i; j++) {
+            cout << ", opt" << j;
+        }
+        cout << ") TraceArr(v, #v, extent<decltype(v)>::value, __LINE__, __FUNCTION__";
+        for (j = 1; j < i; ++j) {
+            cout << ", " << "opt" << j;
+        }
+        cout << ")" << endl;
+    }
 
     return 0;
 }
